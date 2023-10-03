9d96fa75472baaae205d0d59c13939903080e71d
Felix  Osei Danso <felix.osei@amalitech.com>
2023-10-03 08:07:08 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon sanderson',
+            age: 45
+        }
+    }
+}) 
+app.mount('#app')
