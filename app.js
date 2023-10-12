96b1623310fc957614cde551c003df5a4ea7837a
Felix  Osei Danso <felix.osei@amalitech.com>
2023-10-12 08:18:45 +0000

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data(){
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks:true,
             books: [
-                {tittle: 'name of the wind', author: 'patrick rothfuss'},
-                {tittle: 'The of kings', author: 'brandon sanderson'},
-                {tittle: 'The final empire', author: 'patrick rothfuss'}
+                {tittle: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
+                {tittle: 'The of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
+                {tittle: 'The final empire', author: 'patrick rothfuss', img: 'assets/3.jpg'}
             ]
         }
     },
