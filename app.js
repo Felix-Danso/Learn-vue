a10b7a862e308f047a46e573abf2a5a091113f71
Felix  Osei Danso <felix.osei@amalitech.com>
2023-10-12 08:56:11 +0000

    Dynamic classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks:true,
             books: [
-                {tittle: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
-                {tittle: 'The of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
-                {tittle: 'The final empire', author: 'patrick rothfuss', img: 'assets/3.jpg'}
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'The of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+                {title: 'The final empire', author: 'patrick rothfuss', img: 'assets/3.jpg', isFav: true}
             ]
         }
     },
